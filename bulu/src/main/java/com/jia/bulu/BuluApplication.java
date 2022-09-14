620312d96c18e593d8749900eaaf012ac32aa154
jia <jia@li.com>
2022-09-14 20:35:57 +0800

    first commit

---
+++ bulu/src/main/java/com/jia/bulu/BuluApplication.java
@@ -0,0 +1,19 @@
+package com.jia.bulu;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@ServletComponentScan
+@Slf4j
+@EnableTransactionManagement
+public class BuluApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BuluApplication.class,args);
+        log.info("项目启动成功");
+    }
+}
