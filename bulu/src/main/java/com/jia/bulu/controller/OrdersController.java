da57db3dcabf83f46f7bc50e3f2ef84bd3452ffd
jia <jia@li.com>
2022-09-28 19:05:31 +0800

    six commit

--- bulu/src/main/java/com/jia/bulu/controller/OrdersController.java
+++ bulu/src/main/java/com/jia/bulu/controller/OrdersController.java
@@ -1,15 +1,17 @@
 package com.jia.bulu.controller;
 
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.jia.bulu.common.R;
 import com.jia.bulu.entity.Orders;
 import com.jia.bulu.service.OrdersService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.Map;
 
 @Slf4j
 @RequestMapping("/order")
@@ -31,6 +33,20 @@ public class OrdersController {
 
         return R.success("用户下单成功");
 
+    }
+
+
+    @GetMapping("/page")
+    @Cacheable("orderCache")
+    public R<Page<Orders>> page(int page, int pageSize, Long number,LocalDateTime beginDate,LocalDateTime endTime){
+
+        System.out.println(page);
+        System.out.println(pageSize);
+        System.out.println(number);
+        System.out.println(beginDate);
+        System.out.println(endTime);
+
+        return null;
     }
 
 
